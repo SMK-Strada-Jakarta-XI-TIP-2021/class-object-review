27bce176012dbfc60584661fd066f75293abc02d
limmi26 <kisamakiazuel@gmail.com>
2021-08-27 10:42:39 +0700

    perubahan 1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // Tulis kode di bawah ini
+
+const motor = {
+    motorName: "Aerox",
+    motorModel: 155,
+    motorColor: "White",
+    motorBerat: "125kg",
+    motorDetail: [merek, kapasitas, harga, dimensi]
+    
+    kondisiMotor : function() {
+      return
+};
