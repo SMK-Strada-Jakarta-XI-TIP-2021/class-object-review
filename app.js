a984e8123adedb478e541eca9a791675a01df6a4
unknown <dwitegar611@gmail.com>
2021-08-27 11:50:18 +0700

    push ke-2

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ let motor = {
     cc: 1500,
     knalpot: "akrapovic",
     harga: 100000000,
-    
+    syaratMengemudi:['helm', 'sim', 'stnk', 'platnomor', 'spion'],
+    kondisiKendaraan: "Sangat Baik" || true,
     bunyiKnalpot: function () {
         return this.knalpot + ' berbunyi ' + ' kencang ' + 'ngenggg!!!';
+    },
+    start : function(kondisiKendaraan) {
+        if (kondisiKendaraan !== true) {
+            console.log('motor ini berjalan!' + 'berjalan dengan normal')
+        }
     }
 };
 
@@ -22,6 +28,8 @@ console.log(motor.knalpot);
 console.log(motor.harga);
 console.log(motor.bunyiKnalpot());
 
-var motorBunyiKnalpot= motor.bunyiKnalpot();
+let motorBunyiKnalpot= motor.bunyiKnalpot();
 
 console.log('ini ketika knalpot ' + motorBunyiKnalpot);
+console.log('ini syarat berkendara' + motor.syaratMengemudi)
+console.log('ini kondisi motor ' + motor.kondisiKendaraan)
