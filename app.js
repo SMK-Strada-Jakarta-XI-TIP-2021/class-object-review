3c3f38ddd692050be5103d8a88d7155bb4553f67
novi <novia@gmail.com>
2021-08-27 10:35:21 +0700

    ulangan novi

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // Tulis kode di bawah ini
+// Membuat object literal
+let hewan = {
+    nama: "Kucing",
+    warna: "putih",
+    mata: 2,
+    kaki: 4,
+    tipe: "persia",
+    bagus: true,
+};
+
+console.log(hewan);
+console.log(hewan.warna);
+console.log(hewan.nama);
+
+// Constructor
+class Kucing {
+    constructor(tipeKucing, modelKucing) {
+        this.tipeKucing = tipeKucing;
+        this.modelKucing = modelKucing;
+    }
+}
