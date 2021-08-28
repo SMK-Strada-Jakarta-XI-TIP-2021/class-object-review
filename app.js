782c6533d1df0f8f707621d50a33e135877b3dc7
myairin <irenewindagultom@gmail.com>
2021-08-28 13:55:48 +0700

    revisi I

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // Tulis kode di bawah ini
+
+// Mampu membuat `object` dan `object literal`
+
+var hewan = {
+    nama: 'kelinci',
+    warna: 'coklat',
+    warnaMata: 'biru',
+    umur: '5 tahun',
+    jenis: 'cylindrical',
+    tipeTubuh: 'silindris',   
+    kesehatanKelinci: true,
+    
+    kesehatanKelinci : function() {
+        if (this.kesehatanKelinci === true) {
+            return 'kondisi kelinci sehat'
+        } else {
+            return 'kondisi kelinci kurang sehat'
+        }
+        
+    }
+};
+
+console.log(hewan);
+
+
+
+
+
+//  Mampu membuat `object instance` dari `class`
+
