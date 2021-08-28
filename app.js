02b606a9667a1dcd16bf2504a031f6f9a50690a5
adrigunawan <adrigunawan32@gmail.com>
2021-08-28 10:08:33 +0700

    Perbaikan ke-2

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 // Tulis kode di bawah ini
-console.log ('================ Ulangan Object Literal ================')
+console.log ('================ Ulangan Object Literal ================');
 let kendaraan = {
     nama: "Motor",
     model : "Mio GT",
     tipe : 2010,
     perlengkapanKendaraan : ['SIM','STNK','Helm','Spion','PlatNomor'],
-    kondisiKendaraan : 'Baik' || true,
+    kondisiKendaraan : false,
     suara : function () {
-        return 'Bersuara ' + 'Brummm!!!!'
-    }
+        return 'Bersuara ' + 'Brummm!!!!';
+    },
+
+    start : function () {
+        let kondisi = (this.kondisiKendaraan ? true:false);
+        if (kondisi === true) {
+            console.log ("Motor bisa dijalankan ! " + "Memulai perjalanan dari nol");
+        } else {
+            console.log("Motor rusak ! " + "Motor tidak bisa dijalankan");
+        } 
+        return kondisi;
+    },
+
+    perlengkapanBerkendara: function() {
+        let perlengkapan =  ['SIM','STNK','Helm','Spion','PlatNomor'];
+        if (perlengkapan <= this.perlengkapanKendaraan) {
+            console.log("Perlengkapan berkendara sudah lengkap " + "Cuzz Berangkat");
+        } else {
+            console.log('Perlengkapan berkendara kurang nih ! ' + 'ntar ditilang nangiss');
+        }
+
+        return perlengkapan;
+    },
 }
-
 let suaraKendaraan = kendaraan.suara();
-console.log (`Ini ${kendaraan.nama} ${kendaraan.model} keluaran ===> ${kendaraan.tipe}`);
-console.log (`Perlengkapan yang sudah tersedia ==> ${kendaraan.perlengkapanKendaraan}`);
+console.log (`Ini ${kendaraan.nama} ${kendaraan.model} keluaran ==> ${kendaraan.tipe}`);
 console.log (`${kendaraan.nama} ${kendaraan.model} ini bersuara ${kendaraan.suara()}`);
-console.log (`Kondisi motor ini ==> ${kendaraan.kondisiKendaraan}`)
-
-console.log('========================================================')
+console.log (`Perlengkapan yang sudah tersedia ==> ${kendaraan.perlengkapanKendaraan}`);
+kendaraan.perlengkapanBerkendara();
+console.log (`Kondisi motor Sekarang ==> ${kendaraan.kondisiKendaraan}`);
+kendaraan.start();
+console.log('========================================================');
